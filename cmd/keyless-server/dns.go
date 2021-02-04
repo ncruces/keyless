040092e021eb7915d0971863787e71c41f5815ca
Nuno Cruces <ncruces@users.noreply.github.com>
2021-02-04 14:28:50 +0000

    SOA, TXT, compatibility.

--- cmd/keyless-server/dns.go
+++ cmd/keyless-server/dns.go
@@ -32,6 +32,7 @@ func dnsServe(conn net.PacketConn) {
 		res.header.Authoritative = true
 		res.header.RecursionDesired = header.RecursionDesired
 
+		// only QUERY is implemented
 		if header.OpCode != 0 {
 			res.header.RCode = dnsmessage.RCodeNotImplemented
 			logError(res.send(conn, addr, buf))
@@ -39,83 +40,113 @@ func dnsServe(conn net.PacketConn) {
 		}
 
 		question, err := parser.Question()
+		// refuse no questions
+		if err == dnsmessage.ErrSectionDone {
+			res.header.RCode = dnsmessage.RCodeRefused
+			logError(res.send(conn, addr, buf))
+		}
+		// report error
 		if err != nil {
 			res.header.RCode = dnsmessage.RCodeFormatError
 			logError(res.send(conn, addr, buf))
 			continue
 		}
-
-		if !res.answerQuestion(question) {
-			res.header.RCode = dnsmessage.RCodeNameError
-		} else {
-			res.header.RCode = dnsmessage.RCodeSuccess
-		}
-
+		// answer first question
+		res.header.RCode = res.answerQuestion(question)
 		logError(res.send(conn, addr, buf))
 	}
 }
 
 type response struct {
-	header   dnsmessage.Header
-	question dnsmessage.Question
-	answers  []func(*dnsmessage.Builder) error
+	header    dnsmessage.Header
+	question  dnsmessage.Question
+	answer    func(*dnsmessage.Builder) error
+	authority bool
 }
 
-func (r *response) answerQuestion(question dnsmessage.Question) bool {
+func (r *response) answerQuestion(question dnsmessage.Question) dnsmessage.RCode {
+	// ANY is not implemented
+	if question.Type == dnsmessage.TypeALL {
+		return dnsmessage.RCodeNotImplemented
+	}
+
+	// refuse everything outside the zone
+	var name []byte
+	if question.Name.Length > 0 {
+		copy := question.Name.Data
+		name = copy[:question.Name.Length-1]
+	}
+	if n := bytes.TrimSuffix(name, []byte(config.Domain)); len(n) != len(name) {
+		switch {
+		case len(n) == 0:
+			name = nil
+		case n[len(n)-1] == '.':
+			name = n[:len(n)-1]
+		default:
+			return dnsmessage.RCodeRefused
+		}
+	} else {
+		return dnsmessage.RCodeRefused
+	}
+
+	header := dnsmessage.ResourceHeader{
+		Name:  question.Name,
+		Class: dnsmessage.ClassINET,
+	}
+
 	switch question.Type {
 
 	case dnsmessage.TypeA:
-		name := question.Name.Data
-		size := int(question.Name.Length)
-
-		ip := getIPv4(name[:size-1])
+		ip := getIPv4(name)
 		if ip == nil {
-			return false
+			break
 		}
 
-		header := dnsmessage.ResourceHeader{
-			Name:  question.Name,
-			Type:  dnsmessage.TypeA,
-			Class: dnsmessage.ClassINET,
-			TTL:   7 * 24 * 60 * 60,
-		}
-
-		var res dnsmessage.AResource
+		res := dnsmessage.AResource{}
 		copy(res.A[:], ip)
+		header.TTL = 7 * 86400 // 7 days
 
 		r.question = question
-		r.answers = append(r.answers, func(b *dnsmessage.Builder) error {
+		r.answer = func(b *dnsmessage.Builder) error {
 			return b.AResource(header, res)
-		})
-		return true
+		}
+		return dnsmessage.RCodeSuccess
 
 	case dnsmessage.TypeAAAA:
-		name := question.Name.Data
-		size := int(question.Name.Length)
-
-		ip := getIPv6(name[:size-1])
+		ip := getIPv6(name)
 		if ip == nil {
-			return false
+			break
 		}
 
-		header := dnsmessage.ResourceHeader{
-			Name:  question.Name,
-			Type:  dnsmessage.TypeAAAA,
-			Class: dnsmessage.ClassINET,
-			TTL:   7 * 24 * 60 * 60,
-		}
-
-		var res dnsmessage.AAAAResource
+		res := dnsmessage.AAAAResource{}
 		copy(res.AAAA[:], ip)
+		header.TTL = 7 * 86400 // 7 days
 
 		r.question = question
-		r.answers = append(r.answers, func(b *dnsmessage.Builder) error {
+		r.answer = func(b *dnsmessage.Builder) error {
 			return b.AAAAResource(header, res)
-		})
-		return true
+		}
+		return dnsmessage.RCodeSuccess
+
+	case dnsmessage.TypeTXT:
+		if string(name) != "_acme-challenge" {
+			break
+		}
+
+		res := dnsmessage.TXTResource{}
+		header.TTL = 60 // 1 minute
+
+		r.question = question
+		r.answer = func(b *dnsmessage.Builder) error {
+			return b.TXTResource(header, res)
+		}
+		return dnsmessage.RCodeSuccess
 	}
 
-	return false
+	// NXDOMAIN with authority for everything else
+	r.authority = true
+	r.question = question
+	return dnsmessage.RCodeNameError
 }
 
 func (r *response) send(conn net.PacketConn, addr net.Addr, buf []byte) error {
@@ -124,12 +155,17 @@ func (r *response) send(conn net.PacketConn, addr net.Addr, buf []byte) error {
 	builder := dnsmessage.NewBuilder(buf, r.header)
 	builder.EnableCompression()
 
-	err := r.sendQuestions(&builder)
+	err := r.sendQuestion(&builder)
 	if err != nil {
 		return err
 	}
 
-	err = r.sendAnswers(&builder)
+	err = r.sendAnswer(&builder)
+	if err != nil {
+		return err
+	}
+
+	err = r.sendAuthority(&builder)
 	if err != nil {
 		return err
 	}
@@ -149,7 +185,7 @@ func (r *response) send(conn net.PacketConn, addr net.Addr, buf []byte) error {
 	return err
 }
 
-func (r *response) sendQuestions(builder *dnsmessage.Builder) error {
+func (r *response) sendQuestion(builder *dnsmessage.Builder) error {
 	if r.question.Type == 0 {
 		return nil
 	}
@@ -162,56 +198,72 @@ func (r *response) sendQuestions(builder *dnsmessage.Builder) error {
 	return builder.Question(r.question)
 }
 
-func (r *response) sendAnswers(builder *dnsmessage.Builder) error {
+func (r *response) sendAnswer(builder *dnsmessage.Builder) error {
+	if r.answer == nil {
+		return nil
+	}
+
 	err := builder.StartAnswers()
 	if err != nil {
 		return err
 	}
 
-	for _, q := range r.answers {
-		if err := q(builder); err != nil {
-			return err
-		}
-	}
-	return nil
+	return r.answer(builder)
 }
 
-func getIPv4(name []byte) net.IP {
-	i := bytes.IndexByte(name, '.')
-	if i <= 0 || string(name[i+1:]) != config.Domain {
+func (r *response) sendAuthority(builder *dnsmessage.Builder) error {
+	if !r.authority {
 		return nil
 	}
 
-	if string(name[:i]) == "local" {
+	err := builder.StartAuthorities()
+	if err != nil {
+		return err
+	}
+
+	ns := dnsmessage.MustNewName(config.Domain + ".")
+
+	return builder.SOAResource(dnsmessage.ResourceHeader{
+		Name:  r.question.Name,
+		Class: dnsmessage.ClassINET,
+		TTL:   7 * 86400, // 7 days
+	}, dnsmessage.SOAResource{
+		NS:   ns,
+		MBox: ns,
+		// https://www.ripe.net/publications/docs/ripe-203
+		Refresh: 86400,
+		Retry:   7200,
+		Expire:  3600000,
+		MinTTL:  3600,
+	})
+}
+
+func getIPv4(name []byte) net.IP {
+	if string(name) == "local" {
 		return net.IPv4(127, 0, 0, 1).To4()
 	}
 
-	for i := range name[:i] {
+	for i := range name {
 		if name[i] == '-' {
 			name[i] = '.'
 		}
 	}
 
-	return net.ParseIP(string(name[:i])).To4()
+	return net.ParseIP(string(name)).To4()
 }
 
 func getIPv6(name []byte) net.IP {
-	i := bytes.IndexByte(name, '.')
-	if i <= 0 || string(name[i+1:]) != config.Domain {
-		return nil
-	}
-
-	if string(name[:i]) == "local" {
+	if string(name) == "local" {
 		return net.IPv6loopback
 	}
 
-	for i := range name[:i] {
+	for i := range name {
 		if name[i] == '-' {
 			name[i] = ':'
 		}
 	}
 
-	return net.ParseIP(string(name[:i])).To16()
+	return net.ParseIP(string(name)).To16()
 }
 
 func logError(err error) {
